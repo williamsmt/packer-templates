c42f912007184457edc78a21e8d2739c5138ac9e
Matt Williams <williamsmt@users.noreply.github.com>
2021-02-08 08:00:20 -0500

    working 2019 wireframe

--- scripts/enable-winrm.ps1
+++ scripts/enable-winrm.ps1
@@ -1,23 +1,20 @@
-<# Init Log #>;
-Start-Transcript -Path 'C:/Automation/setup.txt' -append;
-<#$DebugPreference = 'Continue' #>;
-<#$VerbosePreference = 'Continue'; #>;
-$InformationPreference = 'Continue';
+$ErrorActionPreference = "Stop"
 
-$NetworkListManager = [Activator]::CreateInstance([Type]::GetTypeFromCLSID([Guid]"{DCB00C01-570F-4A9B-8D69-199FDBA5723B}"))
-$Connections = $NetworkListManager.GetNetworkConnections()
-$Connections | ForEach-Object { $_.GetNetwork().SetCategory(1) }
+# Switch network connection to private mode
+# Required for WinRM firewall rules
+$profile = Get-NetConnectionProfile
+Set-NetConnectionProfile -Name $profile.Name -NetworkCategory Private
 
 Enable-PSRemoting -Force
-winrm quickconfig -q
-winrm quickconfig -transport:http
-winrm set winrm/config '@{MaxTimeoutms="1800000"}'
-winrm set winrm/config/winrs '@{MaxMemoryPerShellMB="800"}'
+
+# Enable WinRM service
+winrm quickconfig -quiet
 winrm set winrm/config/service '@{AllowUnencrypted="true"}'
 winrm set winrm/config/service/auth '@{Basic="true"}'
-winrm set winrm/config/client/auth '@{Basic="true"}'
-winrm set winrm/config/listener?Address=*+Transport=HTTP '@{Port="5985"}'
-netsh advfirewall firewall set rule group="Windows Remote Administration" new enable=yes
-netsh advfirewall firewall set rule name="Windows Remote Management (HTTP-In)" new enable=yes action=allow
+
 Set-Service winrm -startuptype "auto"
 Restart-Service winrm
+
+# Reset auto logon count
+# https://docs.microsoft.com/en-us/windows-hardware/customize/desktop/unattend/microsoft-windows-shell-setup-autologon-logoncount#logoncount-known-issue
+Set-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon' -Name AutoLogonCount -Value 0
